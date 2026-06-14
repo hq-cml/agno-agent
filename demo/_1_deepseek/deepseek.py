d5de9322b6e7cd0c70cf708efeb14a1389190462
hq <hq_cml@163.com>
2026-06-14 15:39:58 +0800

    原生ds

---
+++ demo/_1_deepseek/deepseek.py
@@ -0,0 +1,20 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get('DEEPSEEK_API_KEY'),
+    base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-v4-flash",
+    messages=[
+        {"role": "system", "content": "你是一个问答机器人，请回答各类提问"},
+        {"role": "user", "content": "江苏省淮安市有什么好吃的？"},
+    ],
+    stream=False,
+    reasoning_effort="high",
+    extra_body={"thinking": {"type": "enabled"}}
+)
+
+print("系统apiKey:", os.environ.get('DEEPSEEK_API_KEY'))
+print(response.choices[0].message.content)
