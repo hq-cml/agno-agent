8bfdce1b6002ec886f240aa7ac0ee15dc67a643e
hq <hq_cml@163.com>
2026-06-19 20:41:18 +0800

    记忆系统: 定制化MemoryManager&记忆检索

--- demo/_05_agno_memory/memory_03.py
+++ demo/_05_agno_memory/memory_03.py
@@ -21,14 +21,17 @@ myDb = SqliteDb(db_file=DB_PATH)
 myMemManager = MemoryManager(
     model=myModel, # 这需要模型，说明这个Manager可能是一个子agent？
     db=myDb,
+
+    # 定制化记忆需求
     additional_instructions="""
     不能记录用户的以下信息：年龄和性别
     """,
 
-    # 进一步定制化，比如线上线上环境，进制删除记忆
-    delete_memories=False,
-    #update_memories=True,
+    # 进一步定制化，比如某些线上环境，禁止修改
+    # 实际验证下来发现这几个属性，要么有bug，要么LLM可能会绕过，实际就别用了
+    #update_memories=False,
     #add_memories= True,
+    #delete_memories=False,
     #clear_memories= False,
 )
 agent = Agent(
@@ -45,37 +48,48 @@ def store_memory():
     ret = agent.run('''
     1. 李四，男，土木工作者，80岁，江苏人;
     2. 张三，女，Python开发者，60岁，北京人;
-    3. 王五，女，法官，20岁，山东人''')
+    3. 王五，女，法官，20岁，山东人''', user_id="10001")
     print(f"\n回复：{ret.content}\n")
 
 # 查看具体记忆了哪些内容信息
 def see_memory():
-    mems = agent.get_user_memories() #Note: 为什么这么特殊？
+    mems = agent.get_user_memories(user_id="10001")
     pprint.pprint(mems)
 
 first_run = not os.path.exists(DB_PATH)
 if first_run:
     print("【首次运行】告诉Agent信息，写入数据库")
     store_memory()
-    see_memory()
 
-def remove_memory():
+
+def update_memory():
     ret = agent.run('''
-    删除王五的信息''')
+    王五其实是河北人''', user_id="10001")
     print(f"\n回复：{ret.content}\n")
 
-# remove_memory() #Note：为什么不生效
-#
-# ret = agent.run("王五是谁，多大年龄？")
-# print(f"\n回复：{ret.content}\n")
-
-
-def mem_retrive():
+# 记忆检索，三种模式：
+# first_n: 检索出最开始的n条
+# last_n:  检索出最后的n条
+# agentic: 需要搭配query进行，不过deepseek不支持这种模式，因为不支持response_format type
+def mem_retrieve():
     see_memory()
     ret = myMemManager.search_user_memories(
-        user_id="张三",
+        user_id = "10001",
+        limit=1,
+        #retrieval_method="first_n",
         retrieval_method="last_n",
+        # retrieval_method="agentic",
+        # query="王五是哪里人",
     )
     print(f"\n检索结果：{ret}\n") # 为什么不生效
 
-mem_retrive()
+#see_memory()
+
+# update_memory()
+# see_memory()
+
+# ret = agent.run("王五是谁，多大年龄？", user_id="10001")
+# print(f"\n回复：{ret.content}\n")
+
+
+mem_retrieve()
